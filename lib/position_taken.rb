85df3717b54cc26dd231defd641df8ac098f2687
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-09 01:24:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,4 @@
-def position_taken?(board, index)
-  board[index] == ""
-  index == ""
-end
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+  end
 # code your #position_taken? method here!
