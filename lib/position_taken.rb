55250200034065db5bda6104bfdc0dbcd30af52e
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-09 00:42:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,5 @@
 def position_taken?(board, index)
   board[index] = ""
   index == ""
-  if index["X", "O"]
-    puts board = ["", " ", " ", " ", " ", " ", " ", " ", ""]
 end
 # code your #position_taken? method here!
